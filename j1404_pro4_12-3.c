50e850747da2840556ab3a03cd08fea4af1c3795
Seijun <starjun0212@gmail.com>
2017-01-11 15:58:06 +0900

    未完成

---
+++ j1404_pro4_12-3.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define MAX 1000
+int d[MAX];
+
+void downheap(int j,int k)
+{
+  int i,v;
+  v = d[j];
+
+  while(j <= k/2)
+  {
+    i = 2*j;
+
+    if(i < k && d[i] < d[i+1])
+      i++;
+    if(v >= d[i] || i > k)
+      break;
+    d[j] = d[i];
+    d[i] = v;
+    j = i;
+  }
+}
+
+void print_heap(int n)
+{
+  int l,m,a,i,j,k,x;
+  int count = 0;
+  a = 1;
+  l = 0;
+  x = 1;
+
+  while(n/a > 0)
+  {
+    a *= 2;
+    l++;
+  }
+  m = pow(2,l);
+
+  for(i = 0;i < m;i++)
+  {
+    fprintf(stdout,"=");
+  }
+  fprintf(stdout,"\n");
+
+  for(i = 1;i <= l;i++)
+  {
+    for(j = 0;j < m/pow(2,i) -1;j++)
+    {
+      fprintf(stdout,"  ");
+      count++;
+    }
+    for(j = 0;j < pow(2,i);j++)
+    {
+      fprintf(stdout,"%2d",d[++x]);
+      count++;
+      for(k = 0;k < m/pow(2,i-1) - 1;k++)
+      {
+        if(count >= m)
+          break;
+        fprintf(stdout,"  ");
+        count++;
+      }
+    }
+    if(count >= m)
+      break;
+    else
+    {
+      while(count < m)
+        fprintf(stdout,"  ");
+    }
+    fprintf(stdout,"\n");
+
+  }
+  return ;
+}
+
+void heap_sort(int n)
+{
+  int i,j,k,t;
+
+  for(j = n/2;j >= 1;j--)
+  {
+    print_heap(n);
+    downheap(j,n);
+  }
+
+  k = n;
+  while(k > 1)
+  {
+    t = d[1];
+    d[1] = d[k];
+    d[k] = t;
+    downheap(1,--k);
+  }
+}
+
+int main()
+{
+  int n,i,seed;
+  n = 0;
+
+  fprintf(stdout,"Input Random-Seed : ");
+  scanf("%d",&seed);
+
+  fprintf(stdout,"Input the Number of Data : ");
+  scanf("%d",&n);
+  if(n > MAX)
+  {
+    fprintf(stderr,"N is Too Large!\n");
+    exit(1);
+  }
+
+  srand(seed);
+  for(i = 1;i <= n;i++)
+  {
+    d[i] = (rand()*rand() / 1000) % 100;
+  }
+
+  fprintf(stdout,"\nInput Data :\n");
+  for(i = 1;i <= n;i++)
+  {
+    fprintf(stdout,"d[%3d] : data = %5d\n",i,d[i]);
+  }
+
+  heap_sort(n);
+
+  fprintf(stdout,"\nOutput Data :\n");
+  for(i = 1;i <= n;i++)
+  {
+    fprintf(stdout,"d[%3d] : data = %5d\n",i,d[i]);
+  }
+
+  return 0;
+}
